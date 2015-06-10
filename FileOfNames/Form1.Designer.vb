cb06b710132a53592fd18f36533f27e78311796b
julietspringsteen <juliet.springsteen@gmail.com>
2015-06-10 17:32:22 -0500

    I've added the file of names project to the git

---
+++ FileOfNames/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBoxAddNames = New System.Windows.Forms.TextBox()
+        Me.btnStoreDisplay = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(274, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(337, 303)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Add a Name to the Array"
+        '
+        'txtBoxAddNames
+        '
+        Me.txtBoxAddNames.Location = New System.Drawing.Point(141, 35)
+        Me.txtBoxAddNames.Name = "txtBoxAddNames"
+        Me.txtBoxAddNames.Size = New System.Drawing.Size(113, 20)
+        Me.txtBoxAddNames.TabIndex = 2
+        '
+        'btnStoreDisplay
+        '
+        Me.btnStoreDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStoreDisplay.Location = New System.Drawing.Point(71, 123)
+        Me.btnStoreDisplay.Name = "btnStoreDisplay"
+        Me.btnStoreDisplay.Size = New System.Drawing.Size(147, 43)
+        Me.btnStoreDisplay.TabIndex = 3
+        Me.btnStoreDisplay.Text = "&Store and Display"
+        Me.btnStoreDisplay.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(71, 183)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(147, 43)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(71, 242)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(147, 43)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(635, 346)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnStoreDisplay)
+        Me.Controls.Add(Me.txtBoxAddNames)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtBoxAddNames As System.Windows.Forms.TextBox
+    Friend WithEvents btnStoreDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ FileOfNames/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Counter As Integer
+    Private newCounter As Integer
+    Private nameArray(50) As String
+    Private newArray(50) As String
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtBoxAddNames.Clear()
+        ListBox1.Items.Clear()
+
+    End Sub
+
+    Private Sub btnStoreDisplay_Click(sender As Object, e As EventArgs) Handles btnStoreDisplay.Click
+        Dim reader As New System.IO.StreamReader("Names.txt")
+        Dim names As String
+        ' Dim i As Integer
+        Dim newName As String
+        Dim listOfNames As New List(Of String)
+
+
+        If IO.File.Exists("Names.txt") Then
+            names = reader.ReadLine.ToString ' priming read
+
+            Do While reader.Peek <> -1
+                ListBox1.Items.Add(names)
+                nameArray(Counter) = names
+                Counter += 1
+                names = reader.ReadLine.ToString 'stores names to array, shows them in listbox
+            Loop
+            ListBox1.Items.Add(names) ' need to store last item
+            nameArray(Counter) = names
+            Counter += 1
+
+        Else
+            Dim swCreate As IO.StreamWriter = IO.File.CreateText("Names.txt")
+        End If
+
+        reader.Close() ' closes the file
+
+        newName = txtBoxAddNames.Text
+        newArray(newCounter) = newName
+        ListBox1.Items.Add(newArray(newCounter))
+        newCounter += 1
+
+
+    End Sub
+
+End Class
